c9ead134b451cc4126346626fb1cadbeb10bfd81
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2023-10-17 12:24:17 -0400

    Colando valores padrao no DAO e imagens com compatilibidade:

--- orgs-layout/app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
+++ orgs-layout/app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -1,6 +1,7 @@
 package br.com.alura.orgs.dao
 
 import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
@@ -13,7 +14,12 @@ class ProdutosDao {
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Salada de Frutas",
+                descricao = "Laranja, maçã e uva",
+                valor = BigDecimal("40.20")
+            )
+        )
     }
-
 }
