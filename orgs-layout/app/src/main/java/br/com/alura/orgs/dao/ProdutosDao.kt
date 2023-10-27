9de4d0687308d2431583adf5b1ba96398134a13c
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2023-10-27 10:05:20 -0400

    atualização do projeto

--- orgs-layout/app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
+++ orgs-layout/app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -5,21 +5,22 @@ import java.math.BigDecimal
 
 class ProdutosDao {
 
-    fun adiciona(produto: Produto){
+    fun adiciona(produto: Produto) {
         produtos.add(produto)
     }
 
-    fun buscaTodos() : List<Produto> {
+    fun buscaTodos(): List<Produto> {
         return produtos.toList()
     }
 
     companion object {
         private val produtos = mutableListOf<Produto>(
             Produto(
-                nome = "Salada de Frutas",
-                descricao = "Laranja, maçã e uva",
-                valor = BigDecimal("40.20")
+                nome = "Salada de frutas",
+                descricao = "Laranja, maçãs e uva",
+                valor = BigDecimal("19.83")
             )
         )
     }
+
 }
